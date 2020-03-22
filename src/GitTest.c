b930d973c7a1d03e61a22947b158506051da2dc7
Frank Eggink <freggink@gmail.com>
2020-03-22 17:25:31 +0100

    Added some printf statements

--- src/GitTest.c
+++ src/GitTest.c
@@ -13,5 +13,8 @@
 
 int main(void) {
 	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
 	return EXIT_SUCCESS;
 }
